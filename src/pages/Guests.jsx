11281e2439f8f2096dc86a27c0bb272d7d01f042
Aziz Musyaffah F. <azizmusyaffahfelly@gmail.com>
2026-08-13 22:51:57 +0700

    fbbf

--- src/pages/Guests.jsx
+++ src/pages/Guests.jsx
@@ -149,7 +149,7 @@ const Guests = () => {
           closeModal();
           fetchGuests();
         } else {
-          setFormError(response.data.message || "Berhasil menambah data");
+          setFormError("Berhasil menambah data");
         }
       }
     } catch (err) {
